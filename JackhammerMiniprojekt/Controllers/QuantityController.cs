6ad54b78cc66bbc2f2da8503114dcbe043404525
janMagnusBagge <janjohanssen@hotmail.com>
2016-07-21 16:43:47 +0200

    Add controllers for each category

---
+++ JackhammerMiniprojekt/Controllers/QuantityController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JackhammerMiniprojekt.Controllers
+{
+    public class QuantityController : Controller
+    {
+        // GET: Quantity
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
