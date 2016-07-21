53bde102a21c1fb52836813ccc8237fa5af6b187
janMagnusBagge <janjohanssen@hotmail.com>
2016-07-21 16:48:39 +0200

    Highscore was not added

---
+++ JackhammerMiniprojekt/Models/Highscore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JackhammerMiniprojekt.Models
+{
+	public class Highscore
+	{
+		[Key]
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int Score { get; set; }
+		public int Rank { get; set; }
+	}
+}
