8596f4a1753700450a402012c83a0f760419208e
Marla969 <markus.landberg@hotmail.com>
2016-07-22 16:06:24 +0200

    PunctuationController

---
+++ JackhammerMiniprojekt/Controllers/PunctuationController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JackhammerMiniprojekt.Controllers
+{
+    public class PunctuationController : Controller
+    {
+        // GET: Arbitration
+        public ActionResult Index()
+        {
+            // string p = "Hello, world!";
+            // string fp = new string(p.TakeWhile(c => !Char.IsPunctuation(c)).ToArray());
+            string s = "sxrdct?fvzguh,bij.";
+            var sb = new StringBuilder();
+
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                {
+                    sb.Append(c);
+                }
+                else
+                {
+                    sb.Append('*');
+
+                }
+            }
+            s = sb.ToString();
+            return View();
+        }
+    }
+}
