cd81d19e093a8448d0f97ecf488ea88df9a5dcbe
WeirdMagus <Weird_Magus@Hotmail.com>
2016-07-22 15:03:20 +0200

    Added word question with a way too simple check.

--- JackhammerMiniprojekt/Controllers/QuantityController.cs
+++ JackhammerMiniprojekt/Controllers/QuantityController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using JackhammerMiniprojekt.Repositories;
+using JackhammerMiniprojekt.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,17 @@ namespace JackhammerMiniprojekt.Controllers
 {
     public class QuantityController : Controller
     {
+		JackhammerRepository _JHRepo = new JackhammerRepository();
         // GET: Quantity
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+		public ActionResult Index()
+		{
+			return View(_JHRepo.GetWordQuestion());
+		}
+		[HttpPost]
+		public ActionResult Index(WordsQuestionViewModel model)
+		{
+			if (model == null) return View(_JHRepo.GetWordQuestion());
+			return View(_JHRepo.CheckWordsAnswer(model));
+		}
+	}
 }
---
+++ JackhammerMiniprojekt/ViewModels/WordsQuestionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JackhammerMiniprojekt.ViewModels
+{
+	public class WordsQuestionViewModel
+	{
+		public List<string> Words { get; set; }
+		public string Answer { get; set; }
+		public int Points { get; set; }
+		public bool ShowResult { get; set; }
+	}
+}
