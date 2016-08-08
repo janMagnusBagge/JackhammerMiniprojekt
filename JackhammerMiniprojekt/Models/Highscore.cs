331add6403a6b21dc014628c86314df8135c41b4
janMagnusBagge <janjohanssen@hotmail.com>
2016-08-08 16:28:20 +0200

    UppdatedHomeIndexTwoButtonsAndAddTwoFieldsHighscore

--- JackhammerMiniprojekt/Models/Highscore.cs
+++ JackhammerMiniprojekt/Models/Highscore.cs
@@ -13,5 +13,7 @@ namespace JackhammerMiniprojekt.Models
 		public string Name { get; set; }
 		public int Score { get; set; }
 		public int Rank { get; set; }
+		public int Category { get; set; }
+		public DateTime Date { get; set; }
 	}
 }
---
+++ JackhammerMiniprojekt/ViewModels/FiveQuestionsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JackhammerMiniprojekt.ViewModel
+{
+	public class FiveQuestionsViewModel
+	{
+		[Display(Name = "Question")]
+		public string QuestionString { get; set; }
+		[Display(Name = "Answer")]
+		public string Answer { get; set; }
+		[Display(Name = "Category")]
+		public int Category { get; set; } //test
+
+		public int QuestionDone { get; set; }
+		public int Points { get; set; }
+		public int LastQuestion { get; set; }
+		public int CurrentQuestion { get; set; }
+
+	}
+}
