9f71aa829c52955ce5c2dfdf62790ea84d191547
janMagnusBagge <janjohanssen@hotmail.com>
2016-08-09 08:46:25 +0200

    FiveQuestionsAndStartOfHighscore

--- JackhammerMiniprojekt/ViewModels/FiveQuestionsViewModel.cs
+++ JackhammerMiniprojekt/ViewModels/FiveQuestionsViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace JackhammerMiniprojekt.ViewModel
+namespace JackhammerMiniprojekt.ViewModels
 {
 	public class FiveQuestionsViewModel
 	{
