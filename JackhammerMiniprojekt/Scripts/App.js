0933d3816208b344ad51b4fd45f0297dbe956fc6
Marla969 <markus.landberg@hotmail.com>
2016-08-09 10:43:42 +0200

    Chang Punctuation

--- JackhammerMiniprojekt/Scripts/App.js
+++ JackhammerMiniprojekt/Scripts/App.js
@@ -4,11 +4,18 @@
 
 
     QuestionApp.controller("PunctuationController", function ($scope) {
+
+        $scope.score = 0;
         $scope.originalText;
+
+        var testString = "Hej?!";
+        var testArray = testString.split("");
 
         $scope.checkAnswer = function () {
 
-
+            // kolla om userInput är samma som orginalsträngen,
+            // om den är det, returnera true, annars false
         };
+
     });
 })();
